3ac4e26e46747dd5c2be7160c93ebe3ac1515486
zzj <1733124234@qq.com>
2020-08-28 10:27:24 +0800

    分支操作

--- src/main/java/com/xxxx/demo/DemoApplication.java
+++ src/main/java/com/xxxx/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("test1...............");
         System.out.println("test1...............");
+        System.out.println("test1...............");
     }
 
 }
