2d1a41f56250d56d1ad87ad015d5db7fd72e3fa2
zzj <1733124234@qq.com>
2020-08-28 09:37:03 +0800

    推送远程仓库

--- src/main/java/com/xxxx/demo/DemoApplication.java
+++ src/main/java/com/xxxx/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("test1...............");
+        System.out.println("test1...............");
     }
 
 }
